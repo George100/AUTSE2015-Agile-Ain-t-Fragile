73975233611563e3cab429b5c9f0ae55e63f36d5
George100 <George.Chang7@gmail.com>
2015-05-11 16:31:20 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>SERLER</title>
+	</head>
+	
+	<body>
+		<center><h1>Welcome to SERLER</h1>
+
+
+			<h4>Feel free to view data in the repository.</h4>
+			<br>
+			<a href='inputdataform.php'>Input new data</a>
+			<br><br>
+			<a href='searchdataform.php'>Search data</a>
+
+			</center>
+	</body>
+</html>
