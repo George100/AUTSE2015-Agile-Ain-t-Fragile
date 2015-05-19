65f3a3d7c0fbfbe74138f131643d012365606af3
aoensblend <aoensblend@gmail.com>
2015-05-19 21:03:22 +1200

    More stuff
    Added partial searches
    Added the fields

--- searchdataprocess.php
+++ searchdataprocess.php
@@ -4,52 +4,54 @@
 	</head>
 	
 	<body>
-		<center><h1>Search Results</h1></center>
-			<?php
-				require_once('sqlinfo.inc.php');
+		<center>
+			<h1>Search Results</h1>
+				<?php
+					require_once('sqlinfo.inc.php');
+					
+					$connection = @mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
 				
-				$connection = @mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
-			
-				if (!$connection) {
-					echo "<p><center>Database connection failure</center></p>";
-				} else {					
-					$search = $_GET["search"];
-					// tells user to input something to search for, otherwise search for user input.
-					if (empty ($_GET["search"])) {
-						echo "<p><center>Please enter something to search for!</center></p>";
-					} else {
-					
-						echo "<p><center>Search results for: <i>", $search, "</i></center></p>";
-					
-						$query = "select * from $sql_tble where title like '$search'";
-						$result = mysqli_query($connection, $query);
-						
-						if (!$result) {
-							echo "<p>Something is wrong with ", $query, "</p>";
+					if (!$connection) {
+						echo "<p>Database connection failure</p>";
+					} else {					
+						$search = $_GET["search"];
+						// tells user to input something to search for, otherwise search for user input.
+						if (empty ($_GET["search"])) {
+							echo "<p>Please enter something to search for!</p>";
 						} else {
-							// inform users if there are no results, otherwise show results.
-							if (mysqli_num_rows($result)==0) {
-								echo "<p><center>There are no results for this search. Sorry!</center></p>";
+						
+							echo "<p>Search results for: <i>", $search, "</i></p>";
+						
+							$query = "SELECT * from $sql_tble WHERE title LIKE '$search%'";
+							$result = mysqli_query($connection, $query);
+							
+							if (!$result) {
+								echo "<p>Something is wrong with ", $query, "</p>";
 							} else {
-								echo "<center><table border=\"1\">";
-									
-								while ($row = mysqli_fetch_assoc($result)) {
-									echo "<tr>";
-									echo "<td>Title: ", $row["title"], "</td>";
-									echo "<td>First Name: ", $row["fname"], "</td>";
-									echo "<td>Last Name: ", $row["lname"], "</td>";
-									echo "<td>Date Added: ", $row["dateadded"], "</td>";
-									echo "</tr>";
+								// inform users if there are no results, otherwise show results.
+								if (mysqli_num_rows($result)==0) {
+									echo "<p>There are no results for this search. Sorry!</p>";
+								} else {
+									echo "<table border=\"1\">";
+										
+									while ($row = mysqli_fetch_assoc($result)) {
+										echo "<tr>";
+										echo "<td>Title: ", $row["title"], "</td>";
+										echo "<td>First Name: ", $row["fname"], "</td>";
+										echo "<td>Last Name: ", $row["lname"], "</td>";
+										echo "<td>Date Added: ", $row["dateadded"], "</td>";
+										echo "<td>Date Published: ", $row["datepublish"], "</td>";
+										echo "<td>Category: ", $row["category"], "</td>";
+										echo "</tr>";
+									}
 								}
 							}
+							echo "</table>";
 						}
-						echo "</table></center>";
-					}
-				}	
-				mysqli_close($connection);
-			?>	
-		<center>
-		<br><a href="index.php">Return to Home Page</a>
+					}	
+					mysqli_close($connection);
+				?>	
+			<br><a href="index.php">Return to Home Page</a>
 		</center>
 	</body>
 </html>
