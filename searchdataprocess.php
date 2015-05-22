d125834dfc85f6d1a19fc54d6b4152469370d1e5
aoensblend <aoensblend@gmail.com>
2015-05-22 12:45:13 +1200

    Modified for basic search
    Modified for basic search

--- searchdataprocess.php
+++ searchdataprocess.php
@@ -1,76 +1,57 @@
 <html>
 	<head>
-		<title>Search Status Processed</title>
+		<title>SERLER</title>
 	</head>
 	
 	<body>
 		<center>
-			<h1>Search Results</h1>
-				<?php
-					require_once('sqlinfo.inc.php');
-					
-					$connection = @mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
+			<h1>Welcome to SERLER</h1>
+			<h4>Feel free to view data in the repository.</h4>
+			
+			<?php
+				require_once('sqlinfo.inc.php');
 				
-					if (!$connection) {
-						echo "<p>Database connection failure</p>";
-					} else {					
-						$search = $_GET["searchTitle"];
-						$authorFName = $_GET["authorFName"];
-						$authorLName = $_GET["authorLName"];
-						$datePublished = $_GET["searchYear"];
-						$searchCategory = $_GET["searchCategory"];
-						// tells user to input something to search for, otherwise search for user input.
-						if (empty ($_GET["searchTitle"])) {
+				$connection = @mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
+				
+				if (!$connection) {
+					echo "<p>Database connection failure</p>";
+				} else {
+					
+					if (empty ($_GET["searchTitle"])) {
 							echo "<p>Please enter something to search for!</p>";
+					} else {
+						$query = "SELECT * FROM $sql_tble
+									WHERE title LIKE '%$search%'";
+
+						$result = mysqli_query($connection, $query);
+						if (!$result) {
+							echo "<p>Something is wrong with ", $query, "</p>";
 						} else {
-						
-							$query = "SELECT * FROM $sql_tble
-										WHERE title LIKE '%$search%'";
-						
-							if ($authorFName != null) {
-								$query .= "AND fname LIKE '%$authorFName%'";
-							}
-							if ($authorLName != null) {
-								$query .= "AND lname LIKE '%$authorLName%'";
-							}
-							if ($datePublished != 0 || $datePublished != null) {
-								$query .= "AND datepublish LIKE '$datePublished'";
-							}
-							if ($searchCategory != null) {
-								$query .= "AND category LIKE '%$searchCategory%'";
-							}
-							
-							echo "<p>Search results for: <i>", $search, "</i></p>";
-							
-							$result = mysqli_query($connection, $query);
-							
-							if (!$result) {
-								echo "<p>Something is wrong with ", $query, "</p>";
+							// inform users if there are no results, otherwise show results.
+							if (mysqli_num_rows($result)==0) {
+								echo "<p>There are no results for this search. Sorry!</p>";
 							} else {
-								// inform users if there are no results, otherwise show results.
-								if (mysqli_num_rows($result)==0) {
-									echo "<p>There are no results for this search. Sorry!</p>";
-								} else {
-									echo "<table border=\"1\">";
-										
-									while ($row = mysqli_fetch_assoc($result)) {
-										echo "<tr>";
-										echo "<td>Title: ", $row["title"], "</td>";
-										echo "<td>First Name: ", $row["fname"], "</td>";
-										echo "<td>Last Name: ", $row["lname"], "</td>";
-										echo "<td>Date Added: ", $row["dateadded"], "</td>";
-										echo "<td>Date Published: ", $row["datepublish"], "</td>";
-										echo "<td>Category: ", $row["category"], "</td>";
-										echo "</tr>";
-									}
+								echo "<table border=\"1\">";
+									
+								while ($row = mysqli_fetch_assoc($result)) {
+									echo "<tr>";
+									echo "<td>Title: ", $row["title"], "</td>";
+									echo "<td>First Name: ", $row["fname"], "</td>";
+									echo "<td>Last Name: ", $row["lname"], "</td>";
+									echo "<td>Date Added: ", $row["dateadded"], "</td>";
+									echo "<td>Date Published: ", $row["datepublish"], "</td>";
+									echo "<td>Category: ", $row["category"], "</td>";
+									echo "</tr>";
 								}
 							}
-							echo "</table>";
 						}
-					}	
-					mysqli_close($connection);
-				?>	
-			<br><a href="index.php">Return to Home Page</a>
+						echo "</table>";
+					}
+				}			
+			?>
+			<a href="searchdataformadv.php">Click here to use the advanced search!</a>
+			<br><a href="inputdataform.php">Click here to submit data! (registered users only)</a>
+			<br><a href="index.php">Click here to return to Home Page</a>
 		</center>
 	</body>
 </html>
