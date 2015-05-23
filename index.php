3a638f83cca4afce515b23ffd6e281e2949f9c9b
sammy105 <samratpmw@gmail.com>
2015-05-23 18:24:07 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,48 @@
 <html>
 	<head>
 		<title>SERLER Home</title>
-	</head>
-	
-	<body>
-		<center>
-			<h1>Welcome to SERLER</h1>
-			<h4>Feel free to view data in the repository.</h4>
-			
+    <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+	<img src="Images/logo.png" width="322" height="152" alt=""/>
+    <p>&nbsp;</p>
+    <center>
+    <div class="jumbotron">
+      <div class="container">
+     
+		  <h1>Welcome to SERLER</h1>
+          </div>
+          </div>
+          <p>&nbsp;</p>
+
+<div class="jumbotron">
+    <div class="container">
+	  <h4>Feel free to view data in the repository.</h4>
+      </div>
+</div>
+
 			<form action="searchdataprocess.php" method="get">
 				<p>
 				Title: <input type="text" name="searchTitle" placeholder="Title name">
 				<input type="submit" value="Search!">
 			</form>
+      <div class="btn">
+            <div class="container">
 			<a href="searchdataformadv.php">Advanced Search</a>
+            </div></div>
 			<br>
 			 - 			
 			<br>
+      <div class="btn">
+            <div class="container">
 			<a href="inputdataform.php">Click here to submit data! (registered users only)</a>
+             </div></div>
 			<br>
 			 - 			
 			<br>
+      <div class="btn">
+            <div class="container">
 			<a href="library.php">Click here to view library!</a>
-		</center>
+            </div></div>
+	</center>
 	</body>
 </html>
