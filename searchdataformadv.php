f2ee36e5642d61b09c363a4e60d64a279d7fc919
sammy105 <samratpmw@gmail.com>
2015-05-23 18:43:40 +1200

    Update searchdataformadv.php

--- searchdataformadv.php
+++ searchdataformadv.php
@@ -1,14 +1,29 @@
 <html>
 	<head>
 		<title>SERLER Advanced Search</title>
-	</head>
+	 <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+	<img src="Images/logo.png" width="322" height="152" alt=""/>
+    <img src="Images/aaflogo.png" width="322" height="152" alt="" align="right"/> 
+    <p>&nbsp;</p>
 	
 	<body>
 		<center>
-			<h1>Welcome to SERLER</h1>
-			<h4>Feel free to view data in the repository.</h4>
+        <div class="jumbotron">
+      <div class="container">
+			 <h1>Welcome to SERLER</h1>
+          </div>
+          </div>
+          <p>&nbsp;</p>
+
+<div class="jumbotron">
+    <div class="container">
+	  <h4>Feel free to view data in the repository.</h4>
+    </div>
+</div>
+
 			
-			<form action="searchdataprocessadv.php" method="get">
+		  <form action="searchdataprocessadv.php" method="get">
 				Fill in the fields to search:
 				<p>
 				Title: <input type="text" name="searchTitle" placeholder="Title name">
@@ -23,15 +38,15 @@
 				<p>
 				<input type="submit" value="Search!">
 			</form>
-			<a href="index.php">Basic Search</a>
+			<a href="index.php" class="btn">Basic Search</a>
 			<br>
 			 - 
 			<br>
-			<a href="inputdataform.php">Click here to submit data! (registered users only)</a>
+			<a href="inputdataform.php" class="btn">Click here to submit data! (registered users only)</a>
 			<br>
 			 - 
-			<br><a href="library.php">Click here to view library</a>
+			<br><a href="library.php" class="btn">Click here to view library</a>
 			
-		</center>
+    </center>
 	</body>
 </html>
