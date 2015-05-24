131d46bb59d79ceffb7103cf29a730ee195e7971
sammy105 <samratpmw@gmail.com>
2015-05-24 16:39:44 +1200

    Update index.php

--- index.php
+++ index.php
@@ -4,46 +4,43 @@
     <link href="style.css" rel="stylesheet" type="text/css">
     </head>
 	<img src="Images/logo.png" width="322" height="152" alt=""/>
-        <img src="Images/aaflogo.png" width="322" height="152" alt="" align="right"/> 
-    <p>&nbsp;</p>
+    <img src="Images/aaflogo.png" width="322" height="152" alt="" align="right"/> 
+	<p>&nbsp;</p>
     <center>
     <div class="jumbotron">
       <div class="container">
      
 		  <h1>Welcome to SERLER</h1>
-          </div>
-          </div>
+      </div>
+      </div>
           <p>&nbsp;</p>
 
 <div class="jumbotron">
     <div class="container">
 	  <h4>Feel free to view data in the repository.</h4>
-      </div>
+    </div>
 </div>
 
-			<form action="searchdataprocess.php" method="get">
+	  <form action="searchdataprocess.php" method="get">
 				<p>
-				Title: <input type="text" name="searchTitle" placeholder="Title name">
+				Title<span class="text">:</span>
+				<input type="text" name="searchTitle" placeholder="Title name">
 				<input type="submit" value="Search!">
-			</form>
-      <div class="btn">
-            <div class="container">
-			<a href="searchdataformadv.php">Advanced Search</a>
-            </div></div>
-			<br>
+	  </form>
+      
+			<p><a href="searchdataformadv.php" class="btn">Advanced Search</a>			</p>
+			<p><br>
 			 - 			
 			<br>
-      <div class="btn">
-            <div class="container">
-			<a href="inputdataform.php">Click here to submit data! (registered users only)</a>
-             </div></div>
-			<br>
+      
+			<a href="inputdataform.php" class="btn">Click here to submit data! (registered users only)</a>			</p>
+			<p><br>
 			 - 			
 			<br>
-      <div class="btn">
-            <div class="container">
-			<a href="library.php">Click here to view library!</a>
-            </div></div>
+     
+			<a href="library.php" class="btn">Click here to view library!</a></p>
+            
 	</center>
 	</body>
 </html>
+
