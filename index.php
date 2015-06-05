7436960be7d899e884801aef4aed487608e37deb
aoensblend <aoensblend@gmail.com>
2015-06-06 02:04:54 +1200

    removed links because already have some

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>SERLER Home</title>
-        <link href="style.css" rel="stylesheet" type="text/css">
+		<link href="style.css" rel="stylesheet" type="text/css">
 		<div id="menu">
 			<ul id="nav">
 				<li><a href="index.php" >Home</a></li>
@@ -11,36 +11,23 @@
 				<li><a href="inputdataform.php">Submit data!</a>
 			</ul>
 		</div>
-	   <p>&nbsp;</p>
-	   <p>&nbsp;</p>
-	   <h3>
-      <center>Welcome to</center>
-      <br>
-<center>Software Engineering Research Lab Evidence Repository</center>
-      </h3>
-	 <h4 ><center>Feel free to view data in the repository.</center></h4>
-  
-<p>
-  This website is designed as a prototype repository for SERLER.
-  </p>
-  <p>The prototype has these features:
-    <ul>
-      <li><b>Library:</b> Shows all the research papers currently stored in the repository.</li>
-      <li><b>Bibliographic Search:</b> Searches research papers using basic information (title, author and year).</li>
-      <li><b>Methodology Search:</b> Searches research papers using more detailed information (software development methodology, practices being investigated, nature of the participants, research metrics..etc).</li>
-      <li><b>Submit Data:</b> Submits a research paper authored by you or your group. <i>Require login!</i></li>
-    </ul>
-  </p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<h3>
+		<center>Welcome to</center>
+		<br>
+		<center>Software Engineering Research Lab Evidence Repository</center>
+		</h3>
+		<h4 ><center>Feel free to view data in the repository.</center></h4>
 
-<p>
-  Where do you want to go?
-  <ul>
-  <li><a href="library.php">Visit the library</li></a>
-  <li><a href="bibliographicform.php">Perform a bibliographic search</li></a>
-  <li><a href="methodologyform.php">Perform a methodology search</li></a>
-  <li><a href="inputdataform.php">Submit a research paper</li></a>
-  </ul>
-</p>
-	</body>
+		<p>This website is designed as a prototype repository for SERLER.</p>
+		<p>The prototype has these features:
+		<ul>
+		  <li><b>Library:</b> Shows all the research papers currently stored in the repository.</li>
+		  <li><b>Bibliographic Search:</b> Searches research papers using basic information (title, author and year).</li>
+		  <li><b>Methodology Search:</b> Searches research papers using more detailed information (software development methodology, practices being investigated, nature of the participants, research metrics..etc).</li>
+		  <li><b>Submit Data:</b> Submits a research paper authored by you or your group. <i>Require login!</i></li>
+		</ul>
+		</p>
 </html>
 
