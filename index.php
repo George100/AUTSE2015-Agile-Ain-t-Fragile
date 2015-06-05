f677343f3655ce5c693602acf635da5e01ea64e5
aoensblend <aoensblend@gmail.com>
2015-06-05 15:13:27 +1200

    Removed basic search

--- index.php
+++ index.php
@@ -5,16 +5,10 @@
 
 	 <h1>Welcome to SERLER</h1>
 	 <h4>Feel free to view data in the repository.</h4>
-	  
-	<form action="searchdataprocess.php" method="get">
-			<p>Title
-			<input type="text" name="searchTitle" placeholder="Title name">
-			<input type="submit" value="Search!">
-	</form>
       
-	<a href="searchdataformadv.php" class="btn">Advanced Search</a><br />
-	<a href="inputdataform.php" class="btn">Click here to submit data! (registered users only)</a><br />
-	<a href="library.php" class="btn">Click here to view library!</a>
+	<a href="searchdataformadv.php" class="btn">Bibliographic Search</a><br />
+	<a href="inputdataform.php" class="btn">Submit data!</a><br />
+	<a href="library.php" class="btn">Library!</a>
 	</body>
 </html>
 
