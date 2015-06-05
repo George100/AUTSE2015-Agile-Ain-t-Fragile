ea564b4e7360a50dd5cb93f52bc3ae402c28e5a4
aoensblend <aoensblend@gmail.com>
2015-06-06 01:06:00 +1200

    Updated to show new data fields

--- library.php
+++ library.php
@@ -22,7 +22,7 @@
 				require_once('sqlinfo.inc.php');
 				$connection = @mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
 				$maxResults = 0;
-				
+				$resultNum = 1;
 				if (!$connection) {
 					echo "<p>Something is wrong with ", $query, "</p>";
 				} else {
@@ -35,27 +35,63 @@
 						if (mysqli_num_rows($result)==0) {
 							echo "<p>There are no results for this search. Sorry!</p>";
 						} else {
-							echo "<table border=\"1\">";
+							
 							while ($row = mysqli_fetch_assoc($result)) {
 								$authorOne = $row["authoronefname"] . " " . $row["authoronelname"];
 								$authorTwo = $row["authortwofname"] . " " . $row["authortwolname"];
 								$authorThree = $row["authorthreefname"] . " " . $row["authorthreelname"];
+								echo "<center><table border='1' width='900'>";
+								echo "<td colspan='6'><center><b>Information about the article: ", $resultNum, "</b></center>";
 								echo "<tr>";
-								echo "<td><b>Title: </b>", $row["title"], "</td>";
-								echo "<td><b>Author: </b>", $authorOne, ", <br>", $authorTwo, "</td>";
-								echo "<td><b>Author: </b>", $authorThree, ", <br>", $authorFour, "</td>";
-								echo "<td><b>Date Added: </b>", $row["dateadded"], "</td>";
-								echo "<td><b>Date Published: </b>", $row["datepublish"], "</td>";
-								echo "</tr>";
+								echo "<td width='150'><b>Title: </b>", "</td>";
+								echo "<td width='100'><b>Author: </b></td>";
+								echo "<td width='85'><b>Date Added </b></td>";
+								echo "<td width='90'><b>Date Published: </b></td>";
+								echo "<td width='90'><b>Methodology: </b></td>";
+								echo "<td width='200'><b>Practice Investigated: </b></td>";
+								echo "</tr><tr>";
+								echo "<td>", $row["title"], "</td>";
+								echo "<td>", $authorOne, "<br>", $authorTwo, "<br>", $authorThree, "</td>";
+								echo "<td><center>", $row["dateadded"], "</center></td>";
+								echo "<td><center>", $row["datepublish"], "</center></td>";
+								echo "<td>", $row["sdmethodology"], "</td>";
+								echo "<td>", $row["practicesinvestigated"], "</td>";
+								echo "</tr><tr>";
+								echo "<td colspan='6'><center><b>The Pieces of Evidence<b></center></td>";
+								echo "</tr><tr>";								
+								echo "<td width='150'>The result of the study</td>";
+								echo "<td colspan='2'>", $row["contextstudy"], "</td>";
+								echo "<td width='150'>The benefit or outcome being test</td>";
+								echo "<td colspan='2'>", $row["benefitoutcometested"], "</td>";
+								echo "</tr><tr>";
+								echo "<td>The result of the study</td>";
+								echo "<td colspan='2'>", $row["studyresult"], "</td>";
+								echo "<td>The integrity of the implementation of the practice/method</td>";
+								echo "<td colspan='2'>", $row["implementationintegrity"], "</td>";
+								echo "</tr><tr>";
+								echo "<td colspan='6'><b><center>Information about the research design</center></b></td>";
+								echo "</tr><tr>";
+								echo "<td>Nature of the participants</td>";
+								echo "<td colspan='2'>", $row["numberofparticipants"], "</td>";
+								echo "<td>Research Method</td>";
+								echo "<td colspan='2'>", $row["researchmethod"], "</td>";
+								echo "</tr><tr>";
+								echo "<td>Research Question</td>";
+								echo "<td colspan='2'>", $row["researchquestion"], "</td>";
+								echo "<td>Research Metrics</td>";
+								echo "<td colspan='2'>", $row["researchmetrics"], "</td>";
+								echo "</table></center>";								
+								echo "<p></p> --- <p></p>";
 								// Limits the max. results to 25
 								$maxResults++;
+								$resultNum++;
 								if ($maxResults == 25) {
 									break;
 								}
 							}
 						}
 					}
-					echo "</table>";
+					
 				}
 			?>
 		</div>
