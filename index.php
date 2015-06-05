ca5fbc207e242a83fe7d9b573dabc597ce089ee1
George100 <George.Chang7@gmail.com>
2015-06-06 01:11:35 +1200

    Added content and links
    Added some content and links to other pages at homepage.

--- index.php
+++ index.php
@@ -11,12 +11,36 @@
 				<li><a href="inputdataform.php">Submit data!</a>
 			</ul>
 		</div>
-		<h1><center> 
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<h2>Welcome to SERLER</h2>
-		<h4>Feel free to view data in the repository.</h4>
-		</center>
+	   <p>&nbsp;</p>
+	   <p>&nbsp;</p>
+	   <h3>
+      <center>Welcome to</center>
+      <br>
+<center>Software Engineering Research Lab Evidence Repository</center>
+      </h3>
+	 <h4 ><center>Feel free to view data in the repository.</center></h4>
+  
+<p>
+  This website is designed as a prototype repository for SERLER.
+  </p>
+  <p>The prototype has these features:
+    <ul>
+      <li><b>Library:</b> Shows all the research papers currently stored in the repository.</li>
+      <li><b>Bibliographic Search:</b> Searches research papers using basic information (title, author and year).</li>
+      <li><b>Methodology Search:</b> Searches research papers using more detailed information (software development methodology, practices being investigated, nature of the participants, research metrics..etc).</li>
+      <li><b>Submit Data:</b> Submits a research paper authored by you or your group. <i>Require login!</i></li>
+    </ul>
+  </p>
+
+<p>
+  Where do you want to go?
+  <ul>
+  <li><a href="library.php">Visit the library</li></a>
+  <li><a href="bibliographicform.php">Perform a bibliographic search</li></a>
+  <li><a href="methodologyform.php">Perform a methodology search</li></a>
+  <li><a href="inputdataform.php">Submit a research paper</li></a>
+  </ul>
+</p>
 	</body>
 </html>
 
