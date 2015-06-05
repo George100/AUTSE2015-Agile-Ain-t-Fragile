a6bc2708af789794b9ecc8983fcc2716e04ee782
aoensblend <aoensblend@gmail.com>
2015-06-05 19:14:07 +1200

    removed unnecessary stuff

--- methodologyform.php
+++ methodologyform.php
@@ -69,7 +69,7 @@
 		<textarea name="taResult" placeholder="50 Character Limit" maxLength="50" cols="55" rows="2"></textarea>
 		<p>The integrity of the implementation of the practice/method:<br />
 		<textarea name="taIntegrity" placeholder="50 Character Limit" maxLength="50" cols="55" rows="2"></textarea>
-		
+		<p>
 		<b>Information about the research design</b><br />
 		<i>None of the following below are compulsory</i><p>
 		
@@ -90,16 +90,6 @@
 
 		<p>Research Metrics:<br />
 		<textarea name="taResearchMetrics" placeholder="50 Character Limit" maxLength="50" cols="55" rows="2"></textarea>
-<<<<<<< HEAD
-
-=======
-		<br />Nature of the Participants: 
-		<select>
-			<option value="natureSmall">Small Group (<15 people)</option>
-			<option value="natureMedium">Medium Group (<50 people)</option>
-			<option value="natureLarge">Large Group (>50 people)</option>
-		</select><br />
->>>>>>> origin/master
 		<p>
 		<input type="submit" value="Post">
 		<input type="reset" value="Reset">
